8c7b16407b636158d57ab9f6b5805ed2dbe39fa2
Wahab Khan <kwahab789@gmail.com>
2021-12-06 02:45:38 +0500

    conitnue

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Header from "./components/header";
 import Cart from "./components/Cart";
-// import { Route, Routes } from "react-router";
+
 import { CartProvider } from "react-use-cart";
 import { Route, Routes } from "react-router";
 
